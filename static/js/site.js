1c5de1e079fbee4fe91ec20b0533c8167a826b50
Robert Righter <robrighter@gmail.com>
2010-05-09 00:05:49 -0400

    Added twitterlink

--- static/js/site.js
+++ static/js/site.js
@@ -12,9 +12,10 @@ var marked = new function(){
 
 var insertUser = function (user){
     var template = $('.template').clone();
-    template.find('.name a').html(user.screen_name + '<span><strong>' + user.location + '</strong><br/>' + user.description + '</span>');
-    template.find('.name a').attr('href' ,'http://friendagon.com/'+user.screen_name + '/');
-    template.find('img').attr('src' ,user.profile_image_url);
+    template.find('.name a.friendagonlink').html(user.screen_name + '<span><strong>' + user.location + '</strong><br/>' + user.description + '</span>');
+    template.find('.name .friendagonlink').attr('href' ,'http://friendagon.com/'+user.screen_name + '/');
+    template.find('.name .twitterlink').attr('href' ,'http://twitter.com/'+user.screen_name + '/');
+    template.find('img.avatar').attr('src' ,user.profile_image_url);
     template.find('.following .count').html(user.friends_count);
     template.find('.followers .count').html(user.followers_count);
     template.find('.minus').attr('onClick','unfollow("'+ user.screen_name +'",function() {notifyUnfollowed("'+user.screen_name+'");});');
